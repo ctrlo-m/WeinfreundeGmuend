28063319623fb05ad0aa616a7102f61d820c6c85
Hannes-LTC <hannes.bruss@gmail.com>
2021-03-17 10:56:21 +0100

    Hannes Texteingabe Änderung Float

--- client/public/0_hannes_type_text/js/script.js
+++ client/public/0_hannes_type_text/js/script.js
@@ -4,15 +4,45 @@ socket.on('connected', function (msg) {
     console.log(msg);
 });
 
+
 // Your script starts here ------------------------------------------------------
 let randomHue = Math.round(Math.random() * 360);
 let myColor = "hsl(" + randomHue + ", 100%, 50%)";
 
-let c1 = document.getElementById("conlumn1");
+let textfeld1 = document.getElementById("textfeld1");
 
 window.addEventListener("keydown", keydownHandler);
 
+function keydownHandler(e) {
+    // Prevent browser shortcuts like going back in browser history on backspace
+    e.preventDefault();
+
+    // Sending an event
+    socket.emit('serverEvent', {key:e.key, color:myColor});
+}
+
 // Incoming events 
 socket.on('serverEvent', function (message) {
+    console.log("Incoming event: ", message);
+
+    if (message.key.length == 1) {
+        // If it's a single letter -> create new span element and text
+
+        let newSpan = document.createElement('span');
+        newSpan.style.color = message.color;
+        let newLetter = document.createTextNode(message.key);
+        newSpan.appendChild(newLetter);
+        textfeld1.appendChild(newSpan);
+    
+    } else {
+        // Otherwise it's some kind of special letter like Enter or Backspace
+
+        if (message.key == "Backspace") {
+            let lastIndex = textfeld1.childNodes.length - 1;
+            if (lastIndex >= 0) {
+                textfeld1.childNodes[lastIndex].remove();
+            }
+        }
+    }
 
 });
